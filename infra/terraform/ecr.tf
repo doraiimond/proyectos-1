24b9bb5380cbd4139896d1be6ab82f50d0eb38db
doraiimond <carl.acevedom@duocuc.cl>
2026-07-12 21:18:27 -0400

    correcciones

--- infra/terraform/ecr.tf
+++ infra/terraform/ecr.tf
@@ -2,7 +2,7 @@ resource "aws_ecr_repository" "repositories" {
   for_each = toset(local.ecr_repositories)
 
   name                 = each.value
-  image_tag_mutability = "IMMUTABLE"
+  image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
